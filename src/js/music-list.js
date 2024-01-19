94524ab284ac1f28448b4f4153969f3b6363330d
RuguiCon <jgjimenez.tcp32mixc@conamat.edu.mx>
2024-01-18 21:16:28 -0600

    Jueves 18/01/2024 21:16:20
    Termino del proyecto

--- src/js/music-list.js
+++ src/js/music-list.js
@@ -46,6 +46,8 @@ let allMusic = [
     img: "music-6",
     src: "music-6"
   },
+
+  
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
